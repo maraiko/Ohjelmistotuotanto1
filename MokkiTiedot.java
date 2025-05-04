28a1ac1ca8c38ce2613ed637fbc69e0a55c2649b
jumantti <jumpponena@gmail.com>
2025-05-04 14:43:01 +0300

    Update MokkiTiedot.java
    Samat muutokset kuin asiakastiedoissa.

--- MokkiTiedot.java
+++ MokkiTiedot.java
@@ -2,6 +2,9 @@ package com.example.ohjelmistotuotanto;
 
 import java.time.LocalDateTime;
 
+/**
+ * Luokka mökkeihin liittyville tiedoille ja niiden käsittelylle SQL:ssä
+ */
 public class MokkiTiedot {
     private int id;
     private String osoite;
@@ -24,44 +27,56 @@ public class MokkiTiedot {
     public int getId() {
         return id;
     }
+
     public void setId(int id) {
         this.id = id;
     }
+
     public String getOsoite() {
         return osoite;
     }
+
     public void setOsoite(String osoite) {
         this.osoite = osoite;
     }
+
     public boolean getTila() {
         return tila;
     }
+
     public void setTila(boolean tila) {
         this.tila = tila;
     }
+
     public int getHuoneet() {
         return huoneet;
     }
+
     public void setHuoneet(int huoneet) {
         this.huoneet = huoneet;
     }
+
     public int getKoko() {
         return koko;
     }
+
     public void setKoko(int koko) {
         this.koko = koko;
     }
+
     public LocalDateTime getLuotu() {
         return luotu;
     }
+
     public void setLuotu(LocalDateTime luotu) {
         this.luotu = luotu;
     }
+
     public LocalDateTime getPaivitetty() {
         return paivitetty;
     }
+
     public void setPaivitetty(LocalDateTime paivitetty) {
         this.paivitetty = paivitetty;
     }
-
 }
