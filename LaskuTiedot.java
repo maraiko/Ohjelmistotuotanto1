f66d2af9f3b2a281bd4a050554a7c6fa86ecb035
jumantti <jumpponena@gmail.com>
2025-05-04 14:41:25 +0300

    Update LaskuTiedot.java
    Samat muutokset kuin asiakastiedoissa.

--- LaskuTiedot.java
+++ LaskuTiedot.java
@@ -2,6 +2,9 @@ package com.example.ohjelmistotuotanto;
 
 import java.time.LocalDateTime;
 
+/**
+ * Luokka laskuihin liittyville tiedoille ja niiden käsittelylle SQL:ssä
+ */
 public class LaskuTiedot {
     private int laskuId;
     private float hinta;
@@ -20,27 +23,39 @@ public class LaskuTiedot {
     public int getLaskuId() {
         return laskuId;
     }
+
+    public void setLaskuId(int laskuId) {
+        this.laskuId = laskuId;
+    }
+
     public float getHinta() {
         return hinta;
     }
+
     public void setHinta(float hinta) {
         this.hinta = hinta;
     }
+
     public void setLaskutustapa(String laskutustapa) {
         this.laskutustapa = laskutustapa;
     }
+
     public void setErapaiva(LocalDateTime erapaiva) {
         this.erapaiva = erapaiva;
     }
+
     public String getLaskutustapa() {
         return laskutustapa;
     }
+
     public LocalDateTime getErapaiva() {
         return erapaiva;
     }
+
     public boolean getTila() {
         return tila;
     }
+
     public void setTila(boolean tila) {
         this.tila = tila;
     }
