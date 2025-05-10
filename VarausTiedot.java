9efdd408d978c2a67a074ff5f15a624c2dddef0f
ulassi123 <lassiuosukainen2@gmail.com>
2025-05-10 21:34:55 +0300

    Update VarausTiedot.java

--- VarausTiedot.java
+++ VarausTiedot.java
@@ -1,59 +1,100 @@
 package com.example.ohjelmistotuotanto;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 /**
  * Luokka varauksiin liittyville tiedoille ja niiden käsittelylle SQL:ssä
  */
 public class VarausTiedot {
-    private int varausId;
-    private LocalDateTime aloituspaiva;
-    private LocalDateTime lopetuspaiva;
+    private int varausid;
+    private int asiakas_id;
+    private int mokki_id;
+    private int lasku_id;
+    private LocalDate aloituspaiva;
+    private LocalDate lopetuspaiva;
     private LocalDateTime luotu;
     private LocalDateTime paivitetty;
-    private int asiakasId;
-    private int mokkiId;
-    private int laskuId;
+    DbConnect dbConnect = new DbConnect();
+    private String asiakasNimi;
+    private String mokkiOsoite;
 
-    public VarausTiedot(int varausId, LocalDateTime aloituspaiva, LocalDateTime lopetuspaiva, LocalDateTime luotu, LocalDateTime paivitetty, int asiakasId, int mokkiId, int laskuId) {
-        this.varausId = varausId;
+    public VarausTiedot(int varausId, LocalDate aloituspaiva, LocalDate lopetuspaiva,
+                        LocalDateTime luotu, LocalDateTime paivitetty,
+                        int asiakas_id, int mokki_id, int lasku_id) {
+        this.varausid = varausId;
         this.aloituspaiva = aloituspaiva;
         this.lopetuspaiva = lopetuspaiva;
         this.luotu = luotu;
         this.paivitetty = paivitetty;
-        this.asiakasId = asiakasId;
-        this.mokkiId = mokkiId;
-        this.laskuId = laskuId;
+        this.asiakas_id = asiakas_id;
+        this.mokki_id = mokki_id;
+        this.lasku_id = lasku_id;
+        this.asiakasNimi = dbConnect.haeAsiakasNimi(asiakas_id);
+        this.mokkiOsoite = dbConnect.haeMokkiOsoite(mokki_id);
+    }
+
+    public String getAsiakasNimi() {
+        return asiakasNimi;
+    }
+
+    public String getMokkiOsoite() {
+        return mokkiOsoite;
+    }
+
+    public void setAsiakasNimi(String asiakasNimi) {
+        this.asiakasNimi = asiakasNimi;
+    }
+
+    public void setMokkiOsoite(String mokkiOsoite){
+        this.mokkiOsoite = mokkiOsoite;
     }
 
     public int getVarausId() {
-        return varausId;
+        return varausid;
+    }
+    public void setVarausId(int varausid) {
+        this.varausid = varausid;
     }
 
-    public void setVarausId(int varausId) {
-        this.varausId = varausId;
+    public int getAsiakas_id() {
+        return asiakas_id;
+    }
+    public void setAsiakas_id(int asiakas_id) {
+        this.asiakas_id = asiakas_id;
     }
 
-    public LocalDateTime getAloituspaiva() {
+    public int getMokki_id() {
+        return mokki_id;
+    }
+    public void setMokki_id(int mokki_id) {
+        this.mokki_id = mokki_id;
+    }
+
+    public int getLasku_id() {
+        return lasku_id;
+    }
+    public void setLasku_id(int lasku_id) {
+        this.lasku_id = lasku_id;
+    }
+
+    public LocalDate getAloituspaiva() {
         return aloituspaiva;
     }
-
-    public void setAloituspaiva(LocalDateTime aloituspaiva) {
+    public void setAloituspaiva(LocalDate aloituspaiva) {
         this.aloituspaiva = aloituspaiva;
     }
 
-    public LocalDateTime getLopetuspaiva() {
+    public LocalDate getLopetuspaiva() {
         return lopetuspaiva;
     }
-
-    public void setLopetuspaiva(LocalDateTime lopetuspaiva) {
+    public void setLopetuspaiva(LocalDate lopetuspaiva) {
         this.lopetuspaiva = lopetuspaiva;
     }
 
     public LocalDateTime getLuotu() {
         return luotu;
     }
-
     public void setLuotu(LocalDateTime luotu) {
         this.luotu = luotu;
     }
@@ -61,31 +102,7 @@ public class VarausTiedot {
     public LocalDateTime getPaivitetty() {
         return paivitetty;
     }
-
     public void setPaivitetty(LocalDateTime paivitetty) {
         this.paivitetty = paivitetty;
-    }
-
-    public int getAsiakasId() {
-        return asiakasId;
-    }
-
-    public void setAsiakasId(int asiakasId) {
-        this.asiakasId = asiakasId;
-    }
-
-    public int getMokkiId() {
-        return mokkiId;
-    }
-
-    public void setMokkiId(int mokkiId) {
-        this.mokkiId = mokkiId;
-    }
-
-    public int getLaskuId() {
-        return laskuId;
-    }
-    public void setLaskuId(int laskuId) {
-        this.laskuId = laskuId;
     }
 }
